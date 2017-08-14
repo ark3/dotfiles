ec60cb6feeba0c60aa7d4c960f0ae457c504d696
Abhay Saxena <ark3@datawire.io>
2017-08-14 15:31:55 -0400

    Update to color ls for Linux and Mac

--- config/fish/functions/ls.fish
+++ config/fish/functions/ls.fish
@@ -1,4 +1,14 @@
-# Defined in - @ line 0
-function ls --description 'alias ls=ls -sFG'
-	command ls -sFG $argv;
+function ls --description 'alias ls=ls -sF +color'
+  if not set -q __ls_color_flags
+    switch (uname)
+      case Linux
+        set -g __ls_color_flags '--color=auto'
+      case Darwin
+        set -g __ls_color_flags '-G'
+      case '*'
+        set -g __ls_color_flags ''
+    end
+  end
+
+	command ls -sF $__ls_color_flags $argv;
 end
