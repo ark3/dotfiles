d79e89fde5fbf8cb3471cb6af69dc89191bc8903
Abhay Saxena <ark3@datawire.io>
2017-08-16 14:13:25 -0400

    Add hostname to prompt

--- config/fish/functions/fish_right_prompt.fish
+++ config/fish/functions/fish_right_prompt.fish
@@ -65,6 +65,18 @@ function fish_right_prompt --description 'Write out the right side of the prompt
     set -g __fish_git_prompt_color_cleanstate green --bold
   end
 
+  if not set -q __fish_prompt_hostname_short
+    set -g __fish_prompt_hostname_color (set_color 666)
+    switch (hostname)
+      case 'timonium*'
+        set -g __fish_prompt_hostname_short TMN
+      case 'hexagon*'
+        set -g __fish_prompt_hostname_short HEX
+      case '*'
+        set -g __fish_prompt_hostname_short (hostname)
+    end
+  end
+
   #Set the color for the status depending on the value
   set __fish_color_status (set_color -o green)
   if test $stat -gt 0
@@ -86,7 +98,7 @@ function fish_right_prompt --description 'Write out the right side of the prompt
         set __venv_info " $__fish_prompt_normal""["(basename "$VIRTUAL_ENV")"]"
       end
 
-      printf '%s%s%s%s %s%s %s%s' "$__fish_color_status" "$stat" (__fish_git_prompt) "$__venv_info" "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal" (date "+%H:%M:%S")
+      printf '%s%s%s%s %s%s %s%s %s%s' "$__fish_color_status" "$stat" (__fish_git_prompt) "$__venv_info" "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_hostname_color" "$__fish_prompt_hostname_short" "$__fish_prompt_normal" (date "+%H:%M:%S")
 
   end
 end
