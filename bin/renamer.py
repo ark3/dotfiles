f05da370793cc823c898e5c79f6c978e1905ac14
Abhay Saxena <ark3@email.com>
2021-01-07 08:33:58 -0500

    renamer: Bring it up to Python3.6

--- bin/renamer.py
+++ bin/renamer.py
@@ -1,27 +1,35 @@
 #! /usr/bin/env python
 
-# Reads two lists of filenames from the files specified on the command line.
-# These two lists should be the same length. Renames files listed in the first
-# list as the filenames in the corresponding position in the second list.
+usage = """
+Usage: renamer OLDNAMES NEWNAMES
+
+Reads two lists of filenames from the files specified on the command line.
+These two lists should be the same length. Renames files listed in the first
+list as the filenames in the corresponding position in the second list.
+"""
 
 import sys
 import os
 
-onames = open(sys.argv[1], "read").readlines()
-nnames = open(sys.argv[2], "read").readlines()
+if len(sys.argv) != 3:
+    print(usage)
+    exit(3)
+
+onames = open(sys.argv[1]).readlines()
+nnames = open(sys.argv[2]).readlines()
 
 if len(onames) != len(nnames):
-    print "len onames = %d, len nnames = %d" % (len(onames), len(nnames))
-    print "The lists of filenames must match in length."
-    sys.exit(1)
+    print(f"Found {len(onames)} entries in {sys.argv[1]}")
+    print(f"Found {len(nnames)} entries in {sys.argv[2]}")
+    print("The lists of filenames must match in length.")
+    exit(1)
 
-for idx in range(len(onames)):
-    oname = onames[idx][:-1]
-    nname = nnames[idx][:-1]
-
-    if oname != nname:
-        print "%s --> %s" % (oname, nname)
+for oname, nname in zip(onames, nnames):
+    oname = oname[:-1]
+    nname = nname[:-1]
+    if oname and nname and oname != nname:
+        print(f"{oname} --> {nname}")
         try:
             os.rename(oname, nname)
         except os.error as exc:
-            print "failed", exc
+            print(f"failed {exc}")
