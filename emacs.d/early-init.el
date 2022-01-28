3100221d4c563b9fbf2c48e52e1bdabc7a0e7fcf
Abhay Saxena <ark3@email.com>
2022-01-28 15:09:54 -0500

    emacs: Try harder to get fonts right on startup/first window

--- emacs.d/early-init.el
+++ emacs.d/early-init.el
@@ -16,41 +16,47 @@
 
 ;; Set these settings before the GUI frame is created.
 
-(custom-set-variables
- '(tool-bar-mode nil)
- '(scroll-bar-mode nil)
- '(menu-bar-mode t))
+(setq inhibit-splash-screen t)
 
-(setq default-frame-alist
-      (append (list
-               ;; pixels
-               '(internal-border-width . 1) ; pixels
-               ;; characters
-               '(width . 120) '(height . 50))))
+(defun my/set-up-gui-stuff ()
+  "Set up all the GUI-related stuff that may want to be deferred
+until there's an actual GUI to be set up."
+  ;; set-face-attribute leaks implementation junk. In short, it calls a
+  ;; per-attribute helper, and that helper insists on getting a valid font after
+  ;; each call, so if font-a doesn't have a particular weight or whatever,
+  ;; setting the weight will override the font face, etc. Running through these
+  ;; twice should take care of that.
+  ;;
+  ;; See also: https://debbugs.gnu.org/cgi/bugreport.cgi?bug=45920
+  (setq my/fixed-font "Iosevka Term SS09")
+  (dotimes (_ 2)
+    (set-face-attribute 'default nil
+			;; :font "JetBrainsMono Nerd Font"
+			;; :font "Source Code Pro"
+			:font my/fixed-font :width 'expanded :weight 'medium
+			:height 120)
+    (copy-face `default `fixed-pitch)
+    (set-face-attribute 'variable-pitch nil
+			;; :font "ia Writer Duospace"
+			;; :font "PT Sans Caption"
+			:font "Iosevka Aile" :weight 'medium
+			:height 130)
+    )
+  (setq default-frame-alist
+	(list
+	 `(font . ,my/fixed-font)
+	 '(menu-bar-lines . 0)
+	 '(tool-bar-lines . 0)
+	 '(vertical-scroll-bars . nil)
+	 ;; pixels
+	 '(internal-border-width . 1) ; pixels
+	 '(line-spacing . 1)
+	 ;; characters
+	 '(width . 120)
+	 '(height . 50)))
+  (setq initial-frame-alist default-frame-alist)
+  )
 
-(setq-default line-spacing 1)
-
-(setq inhibit-splash-screen t
-      my/fixed-font "Iosevka Term SS09"
-      ;;my/fixed-font "JetBrainsMono Nerd Font"
-      ;;my/fixed-font "Source Code Pro"
-      )
-
-;; set-face-attribute leaks implementation junk. In short, it calls a
-;; per-attribute helper, and that helper insists on getting a valid font after
-;; each call, so if font-a doesn't have a particular weight or whatever, setting
-;; the weight will override the font face, etc. Running through these twice
-;; should take care of that.
-;;
-;; See also https://debbugs.gnu.org/cgi/bugreport.cgi?bug=45920
-(dotimes (_ 2)
-  (set-face-attribute 'default nil
-		      :font my/fixed-font :height 120
-		      :width 'expanded :weight 'medium)
-  (set-face-attribute 'fixed-pitch nil
-		      :font my/fixed-font :height 120
-		      :width 'expanded :weight 'medium)
-  ;;(set-face-attribute 'variable-pitch nil :font "ia Writer Duospace" :height 130)
-  ;;(set-face-attribute 'variable-pitch nil :font "PT Sans Caption" :height 130)
-  (set-face-attribute 'variable-pitch nil :font "Iosevka Aile" :height 130 :weight 'medium)
-)
+(my/set-up-gui-stuff)
+(add-hook 'after-init-hook 'my/set-up-gui-stuff)
+(add-hook 'server-after-make-frame-hook 'my/set-up-gui-stuff)
